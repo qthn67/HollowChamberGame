5b3ad044c8c4bdffdee4e9bc9faffc4d0c1f1ef4
ImEncrypted <imencrypted48@gmail.com>
2025-03-09 16:06:01 +0000

    cosmetic change

--- box.gd
+++ box.gd
@@ -20,7 +20,7 @@ func _process(delta: float) -> void:
 	# print("triggered: ", triggered)
 	# print("displaying: ", displaying)
 	# print("moving_up: ", moving_up)
-	choice_label.position = Vector2(-532, -271)
+	choice_label.position = Vector2(-610, -340)
 	var move = create_tween()  # Create a new tween if needed
 	
 	if(player.stupid_bool2 == true):
@@ -30,7 +30,7 @@ func _process(delta: float) -> void:
 		player.stupid_bool1 == false
 		player.stupid_bool2 == false
 		triggered = false
-		move.tween_property(self, "position", Vector2(964, 541), 1).set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_EXPO)
+		move.tween_property(self, "position", Vector2(964, 545), 1).set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_EXPO)
 		displaying = true
 	
 	if(displaying):
